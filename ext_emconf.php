78900874e7283fab38e7d91d1c1716eeef2cde21
oliklee <oliklee@735d13b6-9817-0410-8766-e36946ffe9aa>
2006-06-22 14:20:40 +0000

    New version: 0.2.2
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/oelib/trunk@1026 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "oelib"
 # 
-# Auto generated 21-06-2006 09:31
+# Auto generated 22-06-2006 16:20
 # 
 # Manual updates:
 # Only the data in the array - anything else is removed by next write
@@ -30,8 +30,8 @@ $EM_CONF[$_EXTKEY] = Array (
 	'author_company' => '',
 	'private' => '',
 	'download_password' => '',
-	'version' => '0.2.0',	// Don't modify this! Managed automatically during upload to repository.
-	'_md5_values_when_last_written' => 'a:6:{s:13:"changelog.txt";s:4:"5fef";s:37:"class.tx_oelib_salutationswitcher.php";s:4:"256e";s:33:"class.tx_oelib_templatehelper.php";s:4:"67aa";s:12:"ext_icon.gif";s:4:"1bdc";s:8:"todo.txt";s:4:"4d87";s:14:"doc/manual.sxw";s:4:"b21d";}',
+	'version' => '0.2.2',	// Don't modify this! Managed automatically during upload to repository.
+	'_md5_values_when_last_written' => 'a:6:{s:13:"changelog.txt";s:4:"4a99";s:37:"class.tx_oelib_salutationswitcher.php";s:4:"256e";s:33:"class.tx_oelib_templatehelper.php";s:4:"2a7e";s:12:"ext_icon.gif";s:4:"1bdc";s:8:"todo.txt";s:4:"4d87";s:14:"doc/manual.sxw";s:4:"b21d";}',
 );
 
 ?>
