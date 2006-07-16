fc0996f83a7547b35d3dd53ce8c9c679f3ecfc91
oliklee <oliklee@735d13b6-9817-0410-8766-e36946ffe9aa>
2006-07-16 11:29:02 +0000

    new version.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/oelib/trunk@3470 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,14 +2,15 @@
 
 ########################################################################
 # Extension Manager/Repository config file for ext: "oelib"
-# 
-# Auto generated 25-06-2006 13:46
-# 
+#
+# Auto generated 16-07-2006 13:24
+#
 # Manual updates:
-# Only the data in the array - anything else is removed by next write
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
 ########################################################################
 
-$EM_CONF[$_EXTKEY] = Array (
+$EM_CONF[$_EXTKEY] = array(
 	'title' => 'Oliver\'s extension library',
 	'description' => 'This extension contains some code that is shared by some of Oliver Klee\'s extensions. It provides helper functions for templating, switching salutations and other stuff.',
 	'category' => 'services',
@@ -19,6 +20,7 @@ $EM_CONF[$_EXTKEY] = Array (
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
+	'loadOrder' => '',
 	'module' => '',
 	'state' => 'beta',
 	'internal' => '',
@@ -27,11 +29,21 @@ $EM_CONF[$_EXTKEY] = Array (
 	'modify_tables' => '',
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
 	'author_company' => '',
-	'private' => '',
-	'download_password' => '',
-	'version' => '0.2.4',	// Don't modify this! Managed automatically during upload to repository.
-	'_md5_values_when_last_written' => 'a:6:{s:13:"changelog.txt";s:4:"3880";s:37:"class.tx_oelib_salutationswitcher.php";s:4:"256e";s:33:"class.tx_oelib_templatehelper.php";s:4:"1a59";s:12:"ext_icon.gif";s:4:"1bdc";s:8:"todo.txt";s:4:"4d87";s:14:"doc/manual.sxw";s:4:"b21d";}',
+	'version' => '0.2.5',
+	'_md5_values_when_last_written' => 'a:6:{s:13:"changelog.txt";s:4:"36ac";s:37:"class.tx_oelib_salutationswitcher.php";s:4:"256e";s:33:"class.tx_oelib_templatehelper.php";s:4:"3d81";s:12:"ext_icon.gif";s:4:"1bdc";s:8:"todo.txt";s:4:"0fe6";s:14:"doc/manual.sxw";s:4:"07d3";}',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'suggests' => array(
+	),
 );
 
 ?>
