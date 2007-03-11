a50aba73185f6ba16ca5f312b60bd8f9027dd0ee
oliklee <oliklee@735d13b6-9817-0410-8766-e36946ffe9aa>
2007-03-11 14:07:58 +0000

    Fix a comment formatting nit.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/oelib/trunk@5188 735d13b6-9817-0410-8766-e36946ffe9aa

--- class.tx_oelib_templatehelper.php
+++ class.tx_oelib_templatehelper.php
@@ -21,6 +21,7 @@
 *
 * This copyright notice MUST APPEAR in all copies of the script!
 ***************************************************************/
+
 /**
  * Class 'tx_oelib_templatehelper' for the 'oelib' extension
  * (taken from the 'seminars' extension).
