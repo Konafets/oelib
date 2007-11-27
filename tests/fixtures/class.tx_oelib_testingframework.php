0cd7c6157544270def8d4436fddb0eef24528c0b
mrimann <mrimann@735d13b6-9817-0410-8766-e36946ffe9aa>
2007-11-27 18:51:37 +0000

    [Bug 1237] Added testing framework, r=Oliver
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/oelib/trunk@7302 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ tests/fixtures/class.tx_oelib_testingframework.php
@@ -0,0 +1,359 @@
+<?php
+/***************************************************************
+* Copyright notice
+*
+* (c) 2007 Mario Rimann (typo3-coding@rimann.org)
+* All rights reserved
+*
+* This script is part of the TYPO3 project. The TYPO3 project is
+* free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* The GNU General Public License can be found at
+* http://www.gnu.org/copyleft/gpl.html.
+*
+* This script is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+
+/**
+ * Class 'tx_oelib_testingframework' for the 'oelib' extension.
+ *
+ * This is mere a class used for unit tests of the 'oelib' extension. Don't
+ * use it for any other purpose.
+ *
+ * @package		TYPO3
+ * @subpackage	tx_oelib
+ * @author		Mario Rimann <typo3-coding@rimann.org>
+ */
+
+final class tx_oelib_testingframework {
+	private $tablePrefix = '';
+	private $allowedTables = array();
+
+	/**
+	 * The constructor for this class.
+	 *
+	 * This testing framework can be instantiated for one extension at a time.
+	 * Example: In your testcase, you'll have something similar to this line of code:
+	 * $this->fixture = new tx_oelib_testingframework('tx_seminars');
+	 * The parameter you provide is the prefix of the table names of that particular
+	 * extension. Like this, we ensure that the testing framework creates and
+	 * deletes records only on table with this prefix.
+	 *
+	 * If you need dummy records on tables of multiple extensions, you'll have to
+	 * instantiate the testing frame work multiple times (once per extension).
+	 *
+	 * @param	string		the table name prefix of the extension for which
+	 * 						this instance of the testing framework should be used
+	 */
+	public function __construct($tablePrefix) {
+		$this->tablePrefix = $tablePrefix;
+		$this->createListOfAllowedTables();
+	}
+
+	/**
+	 * Creates a new dummy record for unit tests.
+	 *
+	 * If no record data for the new array is given, an empty record will be
+	 * created. It will only contain a valid UID and the "is_dummy_record" flag
+	 * set to 1.
+	 *
+	 * Should there be any problem creating the record (wrong table name or a
+	 * problem with the database), 0 instead of a valid UID will be returned.
+	 *
+	 * @param	string		the name of the table on which the record should be created,
+	 * 						must not be empty
+	 * @param	array		associative array that contains the data to save in
+	 * 						the new record, may be empty
+	 *
+	 * @return	integer		the UID of the new record or 0 if there was a problem
+	 * 						and no record was created
+	 */
+	public function createRecord($table, array $recordData = array()) {
+		if (!$this->isTableNameAllowed($table)) {
+			return 0;
+		}
+
+		// Ensures that this account will have the test flag.
+		$recordData['is_dummy_record'] = 1;
+
+		// Stores the record in the database.
+		$dbResult = $GLOBALS['TYPO3_DB']->exec_INSERTquery(
+			$table,
+			$recordData
+		);
+		if ($dbResult) {
+			$result = $GLOBALS['TYPO3_DB']->sql_insert_id();
+		} else {
+			// Something went wront while inserting the record into the DB.
+			$result = 0;
+		}
+
+		return $result;
+	}
+
+	/**
+	 * Deletes a dummy record from the database.
+	 *
+	 * Important: Only dummy records can be deleted with this method. Should there
+	 * for any reason exist a real record with that UID, it won't be deleted.
+	 *
+	 * @param	string		name of the table from which the record should be
+	 * 						deleted, must not be empty
+	 * @param	integer		UID of the record to delete, must be > 0
+	 *
+	 * @return	boolean		true if everything went well (even if no record was
+	 * 						deleted), false otherwise
+	 */
+	public function deleteRecord($table, $uid) {
+		if (!$this->isTableNameAllowed($table)) {
+			return false;
+		}
+
+		$dbResult = $GLOBALS['TYPO3_DB']->exec_DELETEquery(
+			$table,
+			'uid='.$uid.' AND is_dummy_record=1'
+		);
+
+		return (boolean) $dbResult;
+	}
+
+	/**
+	 * Creates a relation between two records on different tables (so called
+	 * m:n relation).
+	 *
+	 * This method returns a boolean true if everything was fine and false if
+	 * something went wrong (table name not allowed or insert query failed for
+	 * some other reason).
+	 *
+	 * @param	string		name of the m:n table to which the record should be
+	 * 						added, must not be empty
+	 * @param	integer		UID of the local table, must be > 0
+	 * @param	integer		UID of the foreign table, must be > 0
+	 *
+	 * @return	boolean		true if the record was properly saved, false otherwise
+	 */
+	public function createRelation($table, $uidLocal, $uidForeign) {
+		if (!$this->isTableNameAllowed($table)) {
+			return false;
+		}
+
+		// Checks that the two given UIDs are valid.
+		if ((intval($uidLocal) == 0)
+			|| (intval($uidForeign) == 0)
+		) {
+			return false;
+		}
+
+		$recordData = array(
+			'uid_local' => $uidLocal,
+			'uid_foreign' => $uidForeign,
+			'is_dummy_record' => 1
+		);
+
+		// Stores the record in the database.
+		$dbResult = $GLOBALS['TYPO3_DB']->exec_INSERTquery(
+			$table,
+			$recordData
+		);
+
+		// Checks whether the insert query was successful.
+		return (boolean) $dbResult;
+	}
+
+	/**
+	 * Deletes a dummy relation from an m:n table in the database.
+	 *
+	 * Important: Only dummy records can be deleted with this method. Should there
+	 * for any reason exist a real record with that combination of local and
+	 * foreign UID, it won't be deleted!
+	 *
+	 * @param	string		name of the table from which the record should be
+	 * 						deleted, must not be empty
+	 * @param	integer		UID on the local table, must be > 0
+	 * @param	integer		UID on the foreign table, must be > 0
+	 *
+	 * @return	boolean		true if everything went well, false otherwise
+	 */
+	public function removeRelation($table, $uidLocal, $uidForeign) {
+		if (!$this->isTableNameAllowed($table)) {
+			return false;
+		}
+
+		$dbResult = $GLOBALS['TYPO3_DB']->exec_DELETEquery(
+			$table,
+			'uid_local='.$uidLocal.' AND uid_foreign='.$uidForeign
+				.' AND is_dummy_record=1'
+		);
+
+		return (boolean) $dbResult;
+	}
+
+	/**
+	 * Deletes all dummy records that have been added through this framework.
+	 * For this, all records with the "is_dummy_record" flag set to 1 will be
+	 * deleted from all tables to which the testing framework has access to.
+	 */
+	public function cleanUp() {
+		foreach ($this->allowedTables as $currentTable) {
+			// Runs a delete query for each allowed table. A "one-query-deletes-them-all"
+			// approach was tested but we didn't find a working solution for that.
+			$GLOBALS['TYPO3_DB']->exec_DELETEquery(
+				$currentTable,
+				'is_dummy_record=1'
+			);
+
+			// Resets the auto increment setting of the current table.
+			$this->resetAutoIncrement($currentTable);
+		}
+	}
+
+
+
+	// ----------------------------------------------------------------------
+	// Various helper functions
+	// ----------------------------------------------------------------------
+
+	/**
+	 * Generates a list of allowed tables to which this instance of the testing
+	 * framework has access to create/remove test records.
+	 *
+	 * The generated list is based on the list of all tables that TYPO3 can
+	 * access (which will be all tables in this database), filtered by prefix of
+	 * the extension to test.
+	 *
+	 * The array with the allowed table names is written directly to
+	 * $this->allowedTables.
+	 */
+	private function createListOfAllowedTables() {
+		$this->allowedTables = array();
+		$allTables = $GLOBALS['TYPO3_DB']->admin_get_tables();
+		$length = strlen($this->tablePrefix);
+
+		foreach ($allTables as $currentTableName) {
+			if (substr_compare(
+					$this->tablePrefix, $currentTableName, 0, $length
+				) == 0
+			) {
+				$this->allowedTables[] = $currentTableName;
+			}
+		}
+	}
+
+	/**
+	 * Checks whether the given table name is in the list of allowed tables for
+	 * this instance of the testing framework.
+	 *
+	 * @param	string		the name of the table to check, must not be empty
+	 *
+	 * @return	boolean		true if the name of the table is in the list of
+	 * 						allowed tables, false otherwise
+	 */
+	private function isTableNameAllowed($table) {
+		return in_array($table, $this->allowedTables);
+	}
+
+	/**
+	 * Counts the records on the table $table that match a given WHERE clause.
+	 *
+	 * @param	string		the name of the table to query, must not be empty
+	 * @param	string		the where part of the query, may be empty (all records
+	 * 						will be counted in that case)
+	 *
+	 * @return	integer		the number of records that have been found
+	 */
+	public function countRecords($table, $whereClause = '1=1') {
+		if (!$this->isTableNameAllowed($table)) {
+			throw new Exception(
+				'The method countRecords() was called with an empty table name or'
+				.' a table name that is not allowed within the current instance'
+				.' of the testing framework.'
+			);
+		}
+
+		$dbResult = $GLOBALS['TYPO3_DB']->exec_SELECTquery(
+			'COUNT(*) AS number',
+			$table,
+			$whereClause
+		);
+		if (!$dbResult) {
+			throw new Exception('There was an error with the database query.');
+		}
+		$row = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($dbResult);
+		if (!$row) {
+			throw new Exception(
+				'There was an error with the result of the database query.'
+			);
+		}
+
+		return intval($row['number']);
+	}
+
+	/**
+	 * Resets the auto increment value for a given table to the highest existing
+	 * UID + 1. This is required to leave the table in the same status that it
+	 * had before adding dummy records.
+	 *
+	 * @param	string		the name of the table on which we're going to reset
+	 * 						the auto increment entry, must not be empty
+	 */
+	public function resetAutoIncrement($table) {
+		if (!$this->isTableNameAllowed($table)) {
+			throw new Exception(
+				'The given table name is invalid. This means it is either empty'
+				.' or not in the list of allowed tables.'
+			);
+		}
+
+		// Exits without any query to the database if the name of the table ends
+		// with "_mm". Tables that match to this rule contain relations and have
+		// no auto increment index. Querying on them would result in a MySQL error.
+		if (substr($table, -3) == '_mm') {
+			return;
+		}
+
+		// Searches for the record with the highes UID in this table.
+		$dbResult = $GLOBALS['TYPO3_DB']->sql_query(
+			'SELECT MAX(uid) AS uid FROM '.$table.';'
+		);
+		if (!$dbResult) {
+			throw new Exception('There was an error with the database query.');
+		}
+		$row = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($dbResult);
+		if (!$row) {
+			throw new Exception(
+				'There was an error with the result of the database query.'
+			);
+		}
+		$newAutoIncrementValue = $row['uid'] + 1;
+
+		// Updates the auto increment index for this table. The index will be set
+		// to one UID above the highest existing UID.
+		$GLOBALS['TYPO3_DB']->sql_query(
+			'ALTER TABLE '.$table.' AUTO_INCREMENT='.$newAutoIncrementValue.';'
+		);
+	}
+
+	/**
+	 * Returns the list of allowed table names.
+	 *
+	 * @return	array		all allowed table names for this instance of the
+	 * 						testing framework
+	 */
+	public function getListOfAllowedTableNames() {
+		return $this->allowedTables;
+	}
+}
+
+if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/seminarst/tests/fixtures/class.tx_oelib_testingframework.php']) {
+	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/seminars/tests/fixtures/class.tx_oelib_testingframework.php']);
+}
+
+?>
---
+++ tests/tx_oelib_testingframework_testcase.php
@@ -0,0 +1,567 @@
+<?php
+/***************************************************************
+* Copyright notice
+*
+* (c) 2007 Mario Rimann (typo3-coding@rimann.org)
+* All rights reserved
+*
+* This script is part of the TYPO3 project. The TYPO3 project is
+* free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* The GNU General Public License can be found at
+* http://www.gnu.org/copyleft/gpl.html.
+*
+* This script is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+/**
+ * Testcase for the testing framework in the 'oelib' extension.
+ *
+ * @package		TYPO3
+ * @subpackage	tx_oelib
+ * @author		Mario Rimann <typo3-coding@rimann.org>
+ */
+
+require_once(t3lib_extMgm::extPath('oelib')
+	.'tests/fixtures/class.tx_oelib_testingframework.php');
+
+class tx_oelib_testingframework_testcase extends tx_phpunit_testcase {
+	private $fixture;
+
+	protected function setUp() {
+		$this->fixture = new tx_oelib_testingframework('tx_oelib');
+	}
+
+	protected function tearDown() {
+		$this->fixture->cleanUp();
+		unset($this->fixture);
+	}
+
+
+
+	// ---------------------------------------------------------------------
+	// Tests regarding createRecord()
+	// ---------------------------------------------------------------------
+
+	public function testCreateRecordOnValidTableWithNoData() {
+		$this->assertNotEquals(
+			0,
+			$this->fixture->createRecord('tx_oelib_test', array())
+		);
+	}
+
+	public function testCreateRecordWithValidData() {
+		$table = 'tx_oelib_test';
+		$title = 'TEST record';
+		$uid = $this->fixture->createRecord(
+			$table,
+			array(
+				'title' => $title
+			)
+		);
+		$this->assertNotEquals(
+			0,
+			$uid
+		);
+
+		$dbResult = $GLOBALS['TYPO3_DB']->exec_SELECTquery(
+			'title',
+			$table,
+			'uid='.$uid
+		);
+
+		if (!$dbResult) {
+			$this->fail('There was an error with the database query.');
+		}
+
+		$row = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($dbResult);
+		if (!$row) {
+			$this->fail('There was an error with the result of the database query.');
+		}
+		$this->assertEquals(
+			$title,
+			$row['title']
+		);
+	}
+
+	public function testCreateRecordOnInvalidTable() {
+		$this->assertEquals(
+			0,
+			$this->fixture->createRecord('tx_oelib_DOESNOTEXIST', array())
+		);
+	}
+
+	public function testCreateRecordWithEmptyTableName() {
+		$this->assertEquals(
+			0,
+			$this->fixture->createRecord('', array())
+		);
+	}
+
+
+
+	// ---------------------------------------------------------------------
+	// Tests regarding deleteRecord()
+	// ---------------------------------------------------------------------
+
+	public function testDeleteRecordOnValidDummyRecord() {
+		$table = 'tx_oelib_test';
+
+		// Creates and directly destroys a dummy record.
+		$uid = $this->fixture->createRecord($table, array());
+		$this->fixture->deleteRecord($table, $uid);
+
+		// Checks whether the record really was removed from the database.
+		$this->assertEquals(
+			0,
+			$this->fixture->countRecords($table, 'uid='.$uid)
+		);
+	}
+
+	public function testDeleteRecordOnInexistentRecord() {
+		$table = 'tx_oelib_test';
+		$uid = 10000;
+
+		// Checks that the record is inexistent before testing on it.
+		$this->assertEquals(
+			0,
+			$this->fixture->countRecords($table, 'uid='.$uid)
+		);
+
+		// Runs our delete function - it should run through and result true even
+		// when it can't delete a record.
+		$this->assertTrue(
+			$this->fixture->deleteRecord($table, $uid)
+		);
+	}
+
+	public function testDeleteRecordOnForeignTable() {
+		$table = 'tx_seminars_seminars';
+		$uid = 10000;
+
+		$this->assertFalse(
+			$this->fixture->deleteRecord($table, $uid)
+		);
+	}
+
+	public function testDeleteRecordOnInexistentTable() {
+		$table = 'tx_oelib_DOESNOTEXIST';
+		$uid = 10000;
+
+		$this->assertFalse(
+			$this->fixture->deleteRecord($table, $uid)
+		);
+	}
+
+	public function testDeleteRecordWithEmptyTableName() {
+		$table = '';
+		$uid = 10000;
+
+		$this->assertFalse(
+			$this->fixture->deleteRecord($table, $uid)
+		);
+	}
+
+	public function testDeleteRecordOnNonTestRecord() {
+		$table = 'tx_oelib_test';
+
+		// Create a new record that looks like a real record, i.e. the is_dummy_record
+		// flag is set to 0.
+		$dbResult = $GLOBALS['TYPO3_DB']->exec_INSERTquery(
+			$table,
+			array(
+				'title' => 'TEST',
+				'is_dummy_record' => 0
+			)
+		);
+
+		if (!$dbResult) {
+			$this->fail('There was an error with the database query.');
+		}
+
+		$uid = $GLOBALS['TYPO3_DB']->sql_insert_id();
+
+		// Checks whether the creation of the record was successful.
+		$this->assertNotEquals(
+			0,
+			$uid
+		);
+
+		// Runs our delete method which should NOT affect the record created above.
+		$this->assertTrue(
+			$this->fixture->deleteRecord($table, $uid)
+		);
+
+		// Checks whether the record still exists.
+		$this->assertEquals(
+			1,
+			$this->fixture->countRecords($table, 'uid='.$uid)
+		);
+
+		// Deletes the record as it will not be caught by the clean up function.
+		$dbResult = $GLOBALS['TYPO3_DB']->exec_DELETEquery(
+			$table,
+			'uid='.$uid.' AND is_dummy_record=0'
+		);
+
+		if (!$dbResult) {
+			$this->fail('There was an error with the database query.');
+		}
+	}
+
+
+
+	// ---------------------------------------------------------------------
+	// Tests regarding createRelation()
+	// ---------------------------------------------------------------------
+
+	public function testCreateRelationWithValidData() {
+		$table = 'tx_oelib_test_article_mm';
+		$uidLocal = 55;
+		$uidForeign = 2000;
+
+		$this->assertTrue(
+			$this->fixture->createRelation($table, $uidLocal, $uidForeign)
+		);
+
+		// Checks whether the record really exists.
+		$this->assertEquals(
+			1,
+			$this->fixture->countRecords(
+				$table,
+				'uid_local='.$uidLocal.' AND uid_foreign='.$uidForeign
+			)
+		);
+	}
+
+	public function testCreateRelationWithInvalidTable() {
+		$table = 'tx_oelib_test_DOESNOTEXIST_mm';
+		$uidLocal = 55;
+		$uidForeign = 2000;
+
+		$this->assertFalse(
+			$this->fixture->createRelation($table, $uidLocal, $uidForeign)
+		);
+	}
+
+	public function testCreateRelationWithEmptyTableName() {
+		$this->assertFalse(
+			$this->fixture->createRelation('', 200, 500)
+		);
+	}
+
+	public function testCreateRelationWithInvalidData() {
+		$table = 'tx_oelib_test_article_mm';
+
+		$this->assertFalse(
+			$this->fixture->createRelation($table, 0, 50)
+		);
+
+		$this->assertFalse(
+			$this->fixture->createRelation($table, 50, 0)
+		);
+	}
+
+
+
+	// ---------------------------------------------------------------------
+	// Tests regarding removeRelation()
+	// ---------------------------------------------------------------------
+
+	public function testRemoveRelationOnValidDummyRecord() {
+		$table = 'tx_oelib_test_article_mm';
+		$uidLocal = 55;
+		$uidForeign = 77;
+
+		// Creates and directly destroys a dummy record.
+		$this->fixture->createRelation($table, $uidLocal, $uidForeign);
+		$this->fixture->removeRelation($table, $uidLocal, $uidForeign);
+
+		// Checks whether the record really was removed from the database.
+		$this->assertEquals(
+			0,
+			$this->fixture->countRecords(
+				$table,
+				'uid_local='.$uidLocal.' AND uid_foreign='.$uidForeign
+			)
+		);
+	}
+
+	public function testRemoveRelationOnInexistentRecord() {
+		$table = 'tx_oelib_test_article_mm';
+		$uidLocal = 10000;
+		$uidForeign = 20000;
+
+		// Checks that the record is inexistent before testing on it.
+		$this->assertEquals(
+			0,
+			$this->fixture->countRecords(
+				$table,
+				'uid_local='.$uidLocal.' AND uid_foreign='.$uidForeign
+			)
+		);
+
+		// Runs our delete function - it should run through and result true even
+		// when it can't delete a record.
+		$this->assertTrue(
+			$this->fixture->removeRelation($table, $uidLocal, $uidForeign)
+		);
+	}
+
+	public function testRemoveRelationOnForeignTable() {
+		$table = 'tx_seminars_seminars_places_mm';
+		$uidLocal = 10000;
+		$uidForeign = 20000;
+
+		$this->assertFalse(
+			$this->fixture->removeRelation($table, $uidLocal, $uidForeign)
+		);
+	}
+
+	public function testRemoveRelationOnInexistentTable() {
+		$table = 'tx_oelib_DOESNOTEXIST';
+		$uidLocal = 10000;
+		$uidForeign = 20000;
+
+		$this->assertFalse(
+			$this->fixture->removeRelation($table, $uidLocal, $uidForeign)
+		);
+	}
+
+	public function testRemoveRelationWithEmptyTableName() {
+		$table = '';
+		$uidLocal = 10000;
+		$uidForeign = 20000;
+
+		$this->assertFalse(
+			$this->fixture->removeRelation($table, $uidLocal, $uidForeign)
+		);
+	}
+
+	public function testRemoveRelationOnRealRecord() {
+		$table = 'tx_oelib_test_article_mm';
+		$uidLocal = 10000;
+		$uidForeign = 20000;
+
+		// Create a new record that looks like a real record, i.e. the is_dummy_record
+		// flag is set to 0.
+		$dbResult = $GLOBALS['TYPO3_DB']->exec_INSERTquery(
+			$table,
+			array(
+				'uid_local' => $uidLocal,
+				'uid_foreign' => $uidForeign,
+				'is_dummy_record' => 0
+			)
+		);
+
+		if (!$dbResult) {
+			$this->fail('There was an error with the database query.');
+		}
+
+		// Runs our delete method which should NOT affect the record created above.
+		$this->assertTrue(
+			$this->fixture->removeRelation($table, $uidLocal, $uidForeign)
+		);
+
+		// Checks whether the record still exists.
+		$this->assertEquals(
+			1,
+			$this->fixture->countRecords(
+				$table,
+				'uid_local='.$uidLocal.' AND uid_foreign='.$uidForeign
+				)
+		);
+
+		// Deletes the record as it will not be caught by the clean up function.
+		$dbResult = $GLOBALS['TYPO3_DB']->exec_DELETEquery(
+			$table,
+			'uid_local='.$uidLocal.' AND uid_foreign='.$uidForeign
+				.' AND is_dummy_record=0'
+		);
+
+		if (!$dbResult) {
+			$this->fail('There was an error with the database query.');
+		}
+	}
+
+
+
+	// ---------------------------------------------------------------------
+	// Tests regarding dropAllDummyRecords()
+	// ---------------------------------------------------------------------
+
+	public function testCleanUp() {
+		// Creates a dummy record.
+		$this->fixture->createRecord('tx_oelib_test');
+
+		// Deletes all dummy records.
+		$this->fixture->cleanUp();
+
+		// Checks whether all dummy records were deleted.
+		$allowedTables = $this->fixture->getListOfAllowedTableNames();
+		foreach ($allowedTables as $currentTable) {
+			$this->assertEquals(
+				0,
+				$this->fixture->countRecords($currentTable, 'is_dummy_record=1'),
+				'Some test records were not deleted from table "'.$currentTable.'"'
+			);
+		}
+	}
+
+
+
+	// ---------------------------------------------------------------------
+	// Tests regarding createListOfAllowedTables()
+	//
+	// The method is called in the constructor of the fixture.
+	// ---------------------------------------------------------------------
+
+	public function testCreateListOfAllowedTablesContainsOurTestTable() {
+		$allowedTables = $this->fixture->getListOfAllowedTableNames();
+		$this->assertContains(
+			'tx_oelib_test',
+			$allowedTables
+		);
+	}
+
+	public function testCreateListOfAllowedTablesDoesNotContainForeignTables() {
+		$allowedTables = $this->fixture->getListOfAllowedTableNames();
+		$this->assertNotContains(
+			'be_users',
+			$allowedTables
+		);
+	}
+
+
+
+	// ---------------------------------------------------------------------
+	// Tests regarding countRecords()
+	// ---------------------------------------------------------------------
+
+	public function testCountRecordsWithEmptyWhereClause() {
+		$table = 'tx_oelib_test';
+
+		$this->assertEquals(
+			0,
+			$this->fixture->countRecords($table, '')
+		);
+	}
+
+	public function testCountRecordsWithEmptyTableName() {
+		$whereClause = 'is_dummy_record=1';
+
+		try {
+			$this->fixture->countRecords('', $whereClause);
+		} catch (Exception $expected) {
+			return;
+		}
+
+		// Fails the test if the expected exception was not raised above.
+		$this->fail('The expected exception was not caught!');
+	}
+
+	public function testCountRecordsWithForeignTableName() {
+		$table = 'tx_seminars_seminars';
+		$whereClause = 'is_dummy_record=1';
+
+		try {
+			$this->fixture->countRecords($table, $whereClause);
+		} catch (Exception $expected) {
+			return;
+		}
+
+		// Fails the test if the expected exception was not raised above.
+		$this->fail('The expected exception was not caught!');
+	}
+
+	public function testCountRecords() {
+		$table = 'tx_oelib_test';
+		$whereClause = 'is_dummy_record=1';
+
+		$this->assertEquals(
+			0,
+			$this->fixture->countRecords($table, $whereClause)
+		);
+		$this->fixture->createRecord($table);
+		$this->assertEquals(
+			1,
+			$this->fixture->countRecords($table, $whereClause)
+		);
+	}
+
+
+
+	// ---------------------------------------------------------------------
+	// Tests regarding resetAutoIncrement()
+	// ---------------------------------------------------------------------
+
+	public function testResetAutoIncrement() {
+		$table = 'tx_oelib_test';
+
+		// Creates and deletes a record and then resets the auto increment.
+		$latestUid = $this->fixture->createRecord($table);
+		$this->fixture->deleteRecord($table, $latestUid);
+		$this->fixture->resetAutoIncrement($table);
+
+		// Checks whether the reset of the auto increment value worked as it
+		// should. After the reset, the auto increment index should be equal
+		// to the UID of the record we created and deleted before.
+		$dbResult = $GLOBALS['TYPO3_DB']->sql_query(
+			'SHOW TABLE STATUS WHERE Name=\''.$table.'\';'
+		);
+		if (!$dbResult) {
+			$this->fail('There was an error with the database query.');
+		}
+
+		$row = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($dbResult);
+		if (!$row) {
+			$this->fail('There was an error with the result of the database query.');
+		}
+		$this->assertEquals(
+			$latestUid,
+			$row['Auto_increment']
+		);
+	}
+
+	public function testResetAutoIncrementWithEmptyTableName() {
+		try {
+			$this->fixture->resetAutoIncrement('');
+		} catch (Exception $expected) {
+			return;
+		}
+
+		// Fails the test if the expected exception was not raised above.
+		$this->fail('The expected exception was not caught!');
+	}
+
+	public function testResetAutoIncrementWithForeignTable() {
+		try {
+			$this->fixture->resetAutoIncrement('tx_seminars_seminars');
+		} catch (Exception $expected) {
+			return;
+		}
+
+		// Fails the test if the expected exception was not raised above.
+		$this->fail('The expected exception was not caught!');
+	}
+
+	public function testResetAutoIncrementWithInexistentTable() {
+		try {
+			$this->fixture->resetAutoIncrement('tx_oelib_DOESNOTEXIST');
+		} catch (Exception $expected) {
+			return;
+		}
+
+		// Fails the test if the expected exception was not raised above.
+		$this->fail('The expected exception was not caught!');
+	}
+}
+
+?>
