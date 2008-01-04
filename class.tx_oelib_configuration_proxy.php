2faf75e92fd777a023296cd2330ba3a34add3b21
sas-me <sas-me@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-01-04 18:21:55 +0000

    [bug 1183] Create an EM configuration proxy, r=Oliver
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/oelib/trunk@7824 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ class.tx_oelib_configuration_proxy.php
@@ -0,0 +1,242 @@
+<?php
+/***************************************************************
+* Copyright notice
+*
+* (c) 2008 Saskia Metzler <saskia@merlin.owl.de> All rights reserved
+*
+* This script is part of the TYPO3 project. The TYPO3 project is
+* free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* The GNU General Public License can be found at
+* http://www.gnu.org/copyleft/gpl.html.
+*
+* This script is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+
+/**
+ * Class 'tx_oelib_configuration_proxy' for the 'oelib' extension.
+ *
+ * This singleton class provides access to an extension's global configuration
+ * and allows to fake global configuration values for testing purposes.
+ *
+ * @package		TYPO3
+ * @subpackage	tx_oelib
+ * @author		Saskia Metzler <saskia@merlin.owl.de>
+ */
+class tx_oelib_configuration_proxy {
+	/** the singleton configuration proxy objects */
+	static $instances = array();
+
+	/**
+	 * stored configuration data for each extension which currently uses the
+	 * configuration proxy
+	 */
+	private $configuration = array();
+
+	/** key of the extension for which the EM configuration is stored */
+	private $extensionKey = '';
+
+	/** whether the configuration is already loaded */
+	private $isConfigurationLoaded = false;
+
+	/**
+	 * Don't call this constructor; use getInstance instead.
+	 *
+	 * @param	string		extension key without the 'tx' prefix, used to
+	 * 						retrieve the EM configuration and as identifier for
+	 * 						an extension's instance of this class, must not be
+	 * 						empty
+	 */
+	private function __construct($extensionKey) {
+		$this->extensionKey = $extensionKey;
+	}
+
+	/**
+	 * Retrieves the singleton configuration proxy instance for the extension
+	 * named $extensionKey. This function usually should be called statically.
+	 *
+	 * @param	string		extension key without the 'tx' prefix, used to
+	 * 						retrieve the EM configuration and as identifier for
+	 * 						an extension's instance of this class, must not be
+	 * 						empty
+	 *
+	 * @return	tx_oelib_configuration_proxy		the singleton configuration
+	 * 						proxy object
+	 */
+	public function &getInstance($extensionKey) {
+		if ($extensionKey == '') {
+			throw new Exception(
+				'tx_oelib_configuration_proxy: The extension key was not set'
+			);
+		}
+
+		if (!is_object($this->instances[$extensionKey])) {
+			$this->instances[$extensionKey]
+				= new tx_oelib_configuration_proxy($extensionKey);
+		}
+
+		return $this->instances[$extensionKey];
+	}
+
+	/**
+	 * Loads the EM configuration for the extionsion key passed via
+	 * getInstance() if the configuration is not yet loaded.
+	 */
+	private function loadConfiguration() {
+		if (!$this->isConfigurationLoaded) {
+			$this->retrieveConfiguration();
+		}
+	}
+
+	/**
+	 * Retrieves the EM configuration for the extionsion key passed via
+	 * getInstance().
+	 *
+	 * This function is accessible for testing purposes. As lazy implementation
+	 * is used, this function might be useful to ensure static test conditions.
+	 */
+	public function retrieveConfiguration() {
+		$this->configuration = unserialize(
+			$GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][$this->extensionKey]
+		);
+		$this->isConfigurationLoaded = true;
+	}
+
+	/**
+	 * Checks whether a certain key exists in an extension's configuration.
+	 *
+	 * @param	string		key to check, must not be empty
+	 *
+	 * @return	boolean		whether $key occurs in the configuration array of
+	 * 						the extension named $this->extensionKey
+	 */
+	private function hasConfigurationValue($key) {
+		$this->loadConfiguration();
+
+		return isset($this->configuration[$key]);
+	}
+
+	/**
+	 * Returns a configuration value.
+	 *
+	 * @param	string		key of the value to get, must not be empty
+	 *
+	 * @return	mixed		configuration value, will be an empty string if the
+	 * 						value does not exist
+	 */
+	private function getConfigurationValue($key) {
+		$this->loadConfiguration();
+
+		if ($this->hasConfigurationValue($key)) {
+			$result = $this->configuration[$key];
+		} else {
+			$result = '';
+		}
+
+		return $result;
+	}
+
+	/**
+	 * Sets a new configuration value.
+	 *
+	 * The configuration setters are intended to be used for testing purposes
+	 * only.
+	 *
+	 * @param	string		key of the value to set, must not be empty
+	 * @param	mixed		value to set
+	 */
+	private function setConfigurationValue($key, $value) {
+		$this->loadConfiguration();
+
+		$this->configuration[$key] = $value;
+	}
+
+	/**
+	 * Returns a string configuration value.
+	 *
+	 * @param	string		key of the value to get, must not be empty
+	 *
+	 * @return	string		configuration value string, might be empty
+	 */
+	public function getConfigurationValueString($key) {
+		return trim((string) $this->getConfigurationValue($key));
+	}
+
+	/**
+	 * Sets a string configuration value.
+	 *
+	 * @param	string		key of the value to set, must not be empty
+	 * @param	string		value to set, may be empty
+	 */
+	public function setConfigurationValueString($key, $value) {
+		$this->setConfigurationValue($key, trim($value));
+	}
+
+	/**
+	 * Returns a boolean configuration value.
+	 *
+	 * @param	string		key of the value to get, must not be empty
+	 *
+	 * @return	boolean		boolean configuration value
+	 */
+	public function getConfigurationValueBoolean($key) {
+		return (boolean) $this->getConfigurationValue($key);
+	}
+
+	/**
+	 * Sets a boolean configuration value.
+	 *
+	 * @param	string		key of the value to set, must not be empty
+	 * @param	boolean		value to set
+	 */
+	public function setConfigurationValueBoolean($key, $value) {
+		$this->setConfigurationValue($key, intval($value));
+	}
+
+	/**
+	 * Returns an integer configuration value.
+	 *
+	 * @param	string		key of the value to get, must not be empty
+	 *
+	 * @return	integer		configuration value
+	 */
+	public function getConfigurationValueInteger($key) {
+		return intval($this->getConfigurationValue($key));
+	}
+
+	/**
+	 * Sets an integer configuration value.
+	 *
+	 * @param	string		key of the value to set, must not be empty
+	 * @param	integer		value to set
+	 */
+	public function setConfigurationValueInteger($key, $value) {
+		$this->setConfigurationValue($key, $value);
+	}
+
+	/**
+	 * Returns an extension's complete configuration.
+	 *
+	 * @return	array		an extension's configuration, empty if the
+	 * 						configuration was not retrieved before
+	 */
+	public function getCompleteConfiguration() {
+		$this->loadConfiguration();
+
+		return $this->configuration;
+	}
+}
+
+if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/oelib/class.tx_oelib_configuration_proxy.php']) {
+	include_once ($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/oelib/class.tx_oelib_configuration_proxy.php']);
+}
+
+?>
---
+++ tests/tx_oelib_configuration_proxy_testcase.php
@@ -0,0 +1,293 @@
+<?php
+/***************************************************************
+* Copyright notice
+*
+* (c) 2008 Saskia Metzler <saskia@merlin.owl.de> All rights reserved
+*
+* This script is part of the TYPO3 project. The TYPO3 project is
+* free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* The GNU General Public License can be found at
+* http://www.gnu.org/copyleft/gpl.html.
+*
+* This script is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+/**
+ * Testcase for the configuration proxy class in the 'oelib' extension.
+ *
+ * @package		TYPO3
+ * @subpackage	tx_oelib
+ * @author		Saskia Metzler <saskia@merlin.owl.de>
+ */
+
+require_once(t3lib_extMgm::extPath('oelib')
+	.'class.tx_oelib_configuration_proxy.php');
+
+define('OELIB_EXTENSION_KEY', 'oelib');
+
+class tx_oelib_configuration_proxy_testcase extends tx_phpunit_testcase {
+	private $fixture;
+
+	private $testConfiguration = array(
+		'testValueString' => 'foo',
+		'testValueEmptyString' => '',
+		'testValuePositiveInteger' => 2,
+		'testValueNegativeInteger' => -1,
+		'testValueZeroInteger' => 0,
+		'testValueTrue' => 1,
+		'testValueFalse' => 0
+	);
+
+	protected function setUp() {
+		$this->fixture = tx_oelib_configuration_proxy::getInstance(OELIB_EXTENSION_KEY);
+		// ensures the same configuration at the beginning of each test
+		$GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][OELIB_EXTENSION_KEY]
+			= serialize($this->testConfiguration);
+		$this->fixture->retrieveConfiguration();
+	}
+
+	protected function tearDown() {
+		unset($this->fixture);
+	}
+
+
+	public function testGetInstance() {
+		$this->assertTrue(is_object($this->fixture));
+	}
+
+	public function testGetInstanceThrowsExeptionIfNoExtensionKeyGiven() {
+		try {
+			tx_oelib_configuration_proxy::getInstance('');
+		} catch (Exception $expected) {
+			return;
+		}
+
+		// Fails the test if the expected exception was not raised above.
+		$this->fail('The expected exception was not caught!');
+	}
+
+	public function testSingleton() {
+		$this->assertSame(
+			$this->fixture,
+			tx_oelib_configuration_proxy::getInstance(OELIB_EXTENSION_KEY)
+		);
+	}
+
+	public function testInstanciateOfAnotherProxyCreatesNewObject() {
+		$otherConfiguration = tx_oelib_configuration_proxy::getInstance('other_extension');
+
+		$this->assertNotSame(
+			$this->fixture,
+			$otherConfiguration
+		);
+	}
+
+	public function testGetCompleteConfigurationReturnsAllTestConfigurationData() {
+		$this->assertEquals(
+			$this->testConfiguration,
+			$this->fixture->getCompleteConfiguration()
+		);
+	}
+
+	public function testGetConfigurationValueStringIfValueExists() {
+		$this->assertEquals(
+			'foo',
+			$this->fixture->getConfigurationValueString('testValueString')
+		);
+	}
+
+	public function testGetConfigurationValueStringReturnsEmptyStringEmptyStringGiven() {
+		$this->assertEquals(
+			'',
+			$this->fixture->getConfigurationValueString('testValueEmptyString')
+		);
+	}
+
+	public function testGetConfigurationValueStringReturnsEmptyStringIfValueNotExists() {
+		$this->assertEquals(
+			'',
+			$this->fixture->getConfigurationValueString('foo')
+		);
+	}
+
+	public function testGetConfigurationValueBooleanTrue() {
+		$this->assertEquals(
+			true,
+			$this->fixture->getConfigurationValueBoolean('testValueTrue')
+		);
+	}
+
+	public function testGetConfigurationValueBooleanFalse() {
+		$this->assertEquals(
+			false,
+			$this->fixture->getConfigurationValueBoolean('testValueFalse')
+		);
+	}
+
+	public function testGetConfigurationValueBooleanFalseIfValueWasNotSet() {
+		$this->assertEquals(
+			false,
+			$this->fixture->getConfigurationValueBoolean('foo')
+		);
+	}
+
+	public function testGetConfigurationValueIntegerIfPositiveIntegerGiven() {
+		$this->assertEquals(
+			2,
+			$this->fixture->getConfigurationValueInteger(
+				'testValuePositiveInteger'
+			)
+		);
+	}
+
+	public function testGetConfigurationValueIntegerIfNegativeIntegerGiven() {
+		$this->assertEquals(
+			-1,
+			$this->fixture->getConfigurationValueInteger(
+				'testValueNegativeInteger'
+			)
+		);
+	}
+
+	public function testGetConfigurationValueIntegerIfZeroGiven() {
+		$this->assertEquals(
+			0,
+			$this->fixture->getConfigurationValueInteger('testValueZeroInteger')
+		);
+	}
+
+	public function testGetConfigurationValueIntegerIfValueNotExists() {
+		$this->assertEquals(
+			0,
+			$this->fixture->getConfigurationValueInteger('foo')
+		);
+	}
+
+	public function testSetConfigurationValueStringChangesAnExistingValue() {
+		$this->fixture->setConfigurationValueString('testValueString', 'bar');
+
+		$this->assertEquals(
+			'bar',
+			$this->fixture->getConfigurationValueString('testValueString')
+		);
+	}
+
+	public function testSetConfigurationValueStringAddsNewValue() {
+		$this->fixture->setConfigurationValueString('testValue', 'foo');
+
+		$this->assertEquals(
+			'foo',
+			$this->fixture->getConfigurationValueString('testValue')
+		);
+	}
+
+	public function testSetConfigurationValueBooleanTrue() {
+		$this->fixture->setConfigurationValueBoolean('testValue', true);
+
+		$this->assertEquals(
+			true,
+			$this->fixture->getConfigurationValueBoolean('testValue')
+		);
+	}
+
+	public function testSetConfigurationValueBooleanFalse() {
+		$this->fixture->setConfigurationValueBoolean('testValue', false);
+
+		$this->assertEquals(
+			false,
+			$this->fixture->getConfigurationValueBoolean('testValue')
+		);
+	}
+
+	public function testSetConfigurationValueIntegerIfValuePositive() {
+		$this->fixture->setConfigurationValueInteger('testValue', 2);
+
+		$this->assertEquals(
+			2,
+			$this->fixture->getConfigurationValueInteger('testValue')
+		);
+	}
+
+	public function testSetConfigurationValueIntegerIfValueNegative() {
+		$this->fixture->setConfigurationValueInteger('testValue', -2);
+
+		$this->assertEquals(
+			-2,
+			$this->fixture->getConfigurationValueInteger('testValue')
+		);
+	}
+
+	public function testSetConfigurationValueIntegerIfZero() {
+		$this->fixture->setConfigurationValueInteger('testValue', 0);
+
+		$this->assertEquals(
+			0,
+			$this->fixture->getConfigurationValueInteger('testValue')
+		);
+	}
+
+	public function testRetrieveConfigurationSetsOriginalValuesAgainIfValueWasChanged() {
+		$this->fixture->setConfigurationValueString('testValueString', 'bar');
+		$this->fixture->retrieveConfiguration();
+
+		$this->assertEquals(
+			$this->testConfiguration,
+			$this->fixture->getCompleteConfiguration()
+		);
+	}
+
+	public function testRetrieveConfigurationSetsOriginalValuesAgainIfValueWasAdded() {
+		$this->fixture->setConfigurationValueString('testValue', 'foo');
+		$this->fixture->retrieveConfiguration();
+
+		$this->assertEquals(
+			'',
+			$this->fixture->getConfigurationValueString('testValue')
+		);
+	}
+
+	public function testRetrieveConfigurationIfThereIsNone() {
+		unset($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][OELIB_EXTENSION_KEY]);
+		$this->fixture->retrieveConfiguration();
+
+		$this->assertFalse(
+			$this->fixture->getCompleteConfiguration()
+		);
+	}
+
+	public function testRetrieveConfigurationIfThereIsNoneAndSetNewConfigurationValue() {
+		unset($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][OELIB_EXTENSION_KEY]);
+		$this->fixture->retrieveConfiguration();
+		$this->fixture->setConfigurationValueString('testValue', 'foo');
+
+		$this->assertEquals(
+			'foo',
+			$this->fixture->getConfigurationValueString('testValue')
+		);
+	}
+
+	public function testInstanciateAnotherProxyAndSetValueNotAffectsThisFixture() {
+		$otherConfiguration = tx_oelib_configuration_proxy::getInstance('other_extension');
+		$otherConfiguration->setConfigurationValueString('testValue', 'foo');
+
+		$this->assertEquals(
+			'foo',
+			$otherConfiguration->getConfigurationValueString('testValue')
+		);
+
+		$this->assertEquals(
+			$this->testConfiguration,
+			$this->fixture->getCompleteConfiguration()
+		);
+	}
+}
+
+?>
