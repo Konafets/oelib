d0e078686539fbd1896719a63855f9ec40b366d9
nielspardon <nielspardon@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-11-04 21:46:48 +0000

    [Bug 2324] Feature: Add a class templateRegistry
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/oelib/trunk@13746 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ class.tx_oelib_templateRegistry.php
@@ -0,0 +1,114 @@
+<?php
+/***************************************************************
+* Copyright notice
+*
+* (c) 2008 Niels Pardon (mail@niels-pardon.de)
+* All rights reserved
+*
+* This script is part of the TYPO3 project. The TYPO3 project is
+* free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* The GNU General Public License can be found at
+* http://www.gnu.org/copyleft/gpl.html.
+*
+* This script is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+
+/**
+ * Class 'tx_oelib_templateRegistry' for the 'oelib' extension.
+ *
+ * This class represents a registry for templates.
+ *
+ * @package TYPO3
+ * @subpackage tx_oelib
+ *
+ * @author Niels Pardon <mail@niels-pardon.de>
+ */
+class tx_oelib_templateRegistry {
+	/**
+	 * @var tx_oelib_templateRegistry the Singleton instance
+	 */
+	private static $instance = null;
+
+	/**
+	 * @var array already created templates (by file name)
+	 */
+	private $templates = array();
+
+	/**
+	 * Frees as much memory that has been used by this object as possible.
+	 */
+	public function __destruct() {
+		$this->templates = array();
+	}
+
+	/**
+	 * Returns an instance of this class.
+	 *
+	 * @return tx_oelib_templateRegistry the current Singleton instance
+	 */
+	public static function getInstance() {
+		if (!self::$instance) {
+			self::$instance = new tx_oelib_templateRegistry();
+		}
+
+		return self::$instance;
+	}
+
+	/**
+	 * Purges the current instance so that getInstance will create a new
+	 * instance.
+	 */
+	public static function purgeInstance() {
+		self::$instance = null;
+	}
+
+	/**
+	 * Retrieves a template by template file name.
+	 *
+	 * @param string the file name of the template to retrieve, must not be empty
+	 *
+	 * @return tx_oelib_template the template for the given template file name
+	 *
+	 * @see getByFile
+	 */
+	public static function get($templateFileName) {
+		return self::getInstance()->getByFileName($templateFileName);
+	}
+
+	/**
+	 * Retrieves a template by template file name.
+	 *
+	 * @param string the file name of the template to retrieve, must not be empty
+	 *
+	 * @return tx_oelib_template the template for the given template file name
+	 */
+	public function getByFileName($templateFileName) {
+		if ($templateFileName == '') {
+			throw new Exception(
+				'The parameter $templateFileName must not be empty.'
+			);
+		}
+
+		if (!isset($this->templates[$templateFileName])) {
+			$template = t3lib_div::makeInstance('tx_oelib_template');
+			$template->processTemplateFromFile($templateFileName);
+			$this->templates[$templateFileName] = $template;
+		}
+
+		return $this->templates[$templateFileName];
+	}
+}
+
+if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/oelib/class.tx_oelib_templateRegistry.php']) {
+	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/oelib/class.tx_oelib_templateRegistry.php']);
+}
+?>
---
+++ tests/tx_oelib_templateRegistry_testcase.php
@@ -0,0 +1,99 @@
+<?php
+/***************************************************************
+* Copyright notice
+*
+* (c) 2008 Niels Pardon (mail@niels-pardon.de)
+* All rights reserved
+*
+* This script is part of the TYPO3 project. The TYPO3 project is
+* free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* The GNU General Public License can be found at
+* http://www.gnu.org/copyleft/gpl.html.
+*
+* This script is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+
+require_once(t3lib_extMgm::extPath('oelib') . 'class.tx_oelib_templateRegistry.php');
+
+/**
+ * Testcase for the tx_oelib_templateRegistry class in the 'oelib' extension.
+ *
+ * @package TYPO3
+ * @subpackage tx_oelib
+ *
+ * @author Niels Pardon <mail@niels-pardon.de>
+ */
+class tx_oelib_templateRegistry_testcase extends tx_phpunit_testcase {
+	public function setUp() {
+	}
+
+	public function tearDown() {
+		tx_oelib_templateRegistry::purgeInstance();
+	}
+
+
+	////////////////////////////////////////////
+	// Tests concerning the Singleton property
+	////////////////////////////////////////////
+
+	public function testGetInstanceReturnsTemplateRegistryInstance() {
+		$this->assertTrue(
+			tx_oelib_templateRegistry::getInstance()
+				instanceof tx_oelib_templateRegistry
+		);
+	}
+
+	public function testGetInstanceTwoTimesReturnsSameInstance() {
+		$this->assertSame(
+			tx_oelib_templateRegistry::getInstance(),
+			tx_oelib_templateRegistry::getInstance()
+		);
+	}
+
+	public function testGetInstanceAfterPurgeInstanceReturnsNewInstance() {
+		$firstInstance = tx_oelib_templateRegistry::getInstance();
+		tx_oelib_templateRegistry::purgeInstance();
+
+		$this->assertNotSame(
+			$firstInstance,
+			tx_oelib_templateRegistry::getInstance()
+		);
+	}
+
+
+	//////////////////////////
+	// Test concerning get()
+	//////////////////////////
+
+	public function testGetForEmptyTemplateFileNameThrowsException() {
+		$this->setExpectedException(
+			'Exception', 'The parameter $templateFileName must not be empty.'
+		);
+
+		tx_oelib_templateRegistry::get('');
+	}
+
+	public function testGetForExistingTemplateFileNameReturnsTemplate() {
+		$this->assertTrue(
+			tx_oelib_templateRegistry::get('EXT:oelib/tests/fixtures/oelib.html')
+				instanceof tx_oelib_template
+		);
+	}
+
+	public function testGetForExistingTemplateFileNameCalledTwoTimesReturnsTheSameInstance() {
+		$this->assertSame(
+			tx_oelib_templateRegistry::get('EXT:oelib/tests/fixtures/oelib.html'),
+			tx_oelib_templateRegistry::get('EXT:oelib/tests/fixtures/oelib.html')
+		);
+	}
+}
+?>
