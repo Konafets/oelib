119b41e2d92c1480a6dcf99a4539ee28ad75ebf9
oliklee <oliklee@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-01-16 14:24:28 +0000

    Add DB result and DB query exceptions and wrappers for executing queries (tx_oelib_Exception_EmptyQueryResult), r=mario
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/oelib/trunk@15717 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ Exception/class.tx_oelib_Exception_EmptyQueryResult.php
@@ -0,0 +1,60 @@
+<?php
+/***************************************************************
+* Copyright notice
+*
+* (c) 2009 Oliver Klee <typo3-coding@oliverklee.de>
+* All rights reserved
+*
+* This script is part of the TYPO3 project. The TYPO3 project is
+* free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* The GNU General Public License can be found at
+* http://www.gnu.org/copyleft/gpl.html.
+*
+* This script is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+
+/**
+ * Class 'tx_oelib_Exception_EmptyQueryResult' for the 'oelib' extension.
+ *
+ * This class represents an exception that should be thrown when a database
+ * query has an empty result, but shouldn't have.
+ *
+ * The exception automatically will use an error message and the last query.
+ *
+ * @package TYPO3
+ * @subpackage tx_oelib
+ *
+ * @author Oliver Klee <typo3-coding@oliverklee.de>
+ */
+class tx_oelib_Exception_EmptyQueryResult extends Exception {
+	/**
+	 * The constructor.
+	 */
+	public function __construct() {
+		$message = 'The database query returned an empty result, but should ' .
+			'have returned a non-empty result.';
+
+		if ($GLOBALS['TYPO3_DB']->store_lastBuiltQuery
+			|| $GLOBALS['TYPO3_DB']->debugOutput
+		) {
+			$message .= LF . 'The last built query:' . LF .
+				$GLOBALS['TYPO3_DB']->debug_lastBuiltQuery;
+		}
+
+		parent::__construct($message);
+	}
+}
+
+if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/oelib/Exception/class.tx_oelib_Exception_EmptyQueryResult.php']) {
+	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/oelib/Exception/class.tx_oelib_Exception_EmptyQueryResult.php']);
+}
+?>
