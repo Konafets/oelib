45fd5852f9ee9a86a364aa3d6f5c69ac844f0ecd
bernd.schoenbach <bernd.schoenbach@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-06-16 12:43:11 +0000

    [Bug 3074] Create a visibility structure, r=oliver
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/oelib/trunk@21472 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ Visibility/class.tx_oelib_Visibility_Node.php
@@ -0,0 +1,141 @@
+<?php
+/***************************************************************
+* Copyright notice
+*
+* (c) 2009 Bernd Schönbach <bernd@oliverklee.de>
+* All rights reserved
+*
+* This script is part of the TYPO3 project. The TYPO3 project is
+* free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* The GNU General Public License can be found at
+* http://www.gnu.org/copyleft/gpl.html.
+*
+* This script is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+
+/**
+ * Class 'tx_oelib_Visibility_Node' for the 'oelib' extension.
+ *
+ * This class represents a node for a visibility tree.
+ *
+ * @package TYPO3
+ * @subpackage tx_oelib
+ *
+ * @author Bernd Schönbach <bernd@oliverklee.de>
+ */
+class tx_oelib_Visibility_Node {
+	/**
+	 * @var array numeric array with all direct children of this node
+	 */
+	private $children = array();
+
+	/**
+	 * @var tx_oelib_Visibility_Node the parent node of this node
+	 */
+	private $parentNode = null;
+
+	/**
+	 * @var boolean whether this node is visible
+	 */
+	private $isVisible;
+
+	/**
+	 * Constructor of this class.
+	 *
+	 * @param boolean $isVisible whether this node should be initially visible
+	 */
+	public function __construct($isVisible = false) {
+		$this->isVisible = $isVisible;
+	}
+
+	/**
+	 * Destructor of this class. Tries to free as much memory as possible.
+	 */
+	public function __destruct() {
+		if ($this->parentNode) {
+			unset($this->parentNode);
+		}
+
+		foreach ($this->children as $key => $child) {
+			$child->__destruct();
+			unset($this->children[$key]);
+		}
+	}
+
+	/**
+	 * Adds a child to this node.
+	 *
+	 * @param tx_oelib_Visibility_Node $child the child to add to this node
+	 */
+	public function addChild(tx_oelib_Visibility_Node $child) {
+		$this->children[] = $child;
+	}
+
+	/**
+	 * Sets the parent node of this node.
+	 *
+	 * The parent can only be set once.
+	 *
+	 * @param tx_oelib_Visibility_Node $parentNode the parent node to add
+	 */
+	public function setParent(tx_oelib_Visibility_Node $parentNode) {
+		if ($this->parentNode instanceof tx_oelib_Visibility_Node) {
+			throw new Exception('This node already has a parent node.');
+		}
+
+		$this->parentNode = $parentNode;
+	}
+
+	/**
+	 * Returns the visibility status of this node.
+	 *
+	 * @return boolean true if this node is visible, false otherwise
+	 */
+	public function isVisible() {
+		return $this->isVisible;
+	}
+
+	/**
+	 * Marks this node as visible and propagates the visibility recursively to
+	 * the parent up to the root.
+	 */
+	public function markAsVisible() {
+		$this->isVisible = true;
+		if ($this->parentNode) {
+			$this->parentNode->markAsVisible();
+		}
+	}
+
+	/**
+	 * Returns the children set for the current node.
+	 *
+	 * @return array this node's children, will be empty if no children are set
+	 */
+	public function getChildren() {
+		return $this->children;
+	}
+
+	/**
+	 * Returns the parent node set for this node.
+	 *
+	 * @return tx_oelib_Visibility_Node the parent node of this node, will be
+	 *                                  empty if no parent was set
+	 */
+	public function getParent() {
+		return $this->parentNode;
+	}
+}
+
+if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/oelib/Visibility/class.tx_oelib_Visibility_Node.php']) {
+	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/oelib/Visibility/class.tx_oelib_Visibility_Node.php']);
+}
+?>
---
+++ tests/tx_oelib_Visibility_Node_testcase.php
@@ -0,0 +1,184 @@
+<?php
+/***************************************************************
+* Copyright notice
+*
+* (c) 2009 Bernd Schönbach <bernd@oliverklee.de>
+* All rights reserved
+*
+* This script is part of the TYPO3 project. The TYPO3 project is
+* free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* The GNU General Public License can be found at
+* http://www.gnu.org/copyleft/gpl.html.
+*
+* This script is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+
+require_once(t3lib_extMgm::extPath('oelib') . 'class.tx_oelib_Autoloader.php');
+
+/**
+ * Testcase for the visibility node class of the 'oelib' extension.
+ *
+ * @package TYPO3
+ * @subpackage tx_oelib
+ *
+ * @author Bernd Schönbach <bernd@oliverklee.de>
+ */
+class tx_oelib_Visibility_Node_testcase extends tx_phpunit_testcase {
+	/**
+	 * @var tx_oelib_visibilityNode
+	 */
+	private $fixture;
+
+	protected function setUp() {
+		$this->fixture = new tx_oelib_Visibility_Node();
+	}
+
+	protected function tearDown() {
+		$this->fixture->__destruct();
+		unset($this->fixture);
+	}
+
+
+	//////////////////////////////
+	// Tests for the constructor
+	//////////////////////////////
+
+	public function test_IsVisible_IfSetToVisibleConstruction_ReturnsVisibilityFromConstruction() {
+		$fixture = new tx_oelib_Visibility_Node(true);
+
+		$this->assertTrue(
+			$fixture->isVisible()
+		);
+
+		$fixture->__destruct();
+	}
+
+	public function test_IsVisible_IfSetToHiddenConstruction_ReturnsVisibilityFromConstruction() {
+		$fixture = new tx_oelib_Visibility_Node(false);
+
+		$this->assertFalse(
+			$fixture->isVisible()
+		);
+
+		$fixture->__destruct();
+	}
+
+
+	//////////////////////////////
+	// Tests concerning addChild
+	//////////////////////////////
+
+	public function test_getChildren_WithoutChildrenSet_ReturnsEmptyArray() {
+		$this->assertEquals(
+			array(),
+			$this->fixture->getChildren()
+		);
+	}
+
+	public function test_addChild_WithOneGivenChildren_AddsOneChildToNode() {
+		$childNode = new tx_oelib_Visibility_Node();
+		$this->fixture->addChild($childNode);
+
+		$this->assertEquals(
+			array($childNode),
+			$this->fixture->getChildren()
+		);
+	}
+
+	public function test_addChild_ForNodeWithOneChildAndAnotherChildGiven_AddsAnotherChildToNode() {
+		$this->fixture->addChild(new tx_oelib_Visibility_Node());
+		$this->fixture->addChild(new tx_oelib_Visibility_Node());
+
+		$this->assertEquals(
+			2,
+			count($this->fixture->getChildren())
+		);
+	}
+
+
+	///////////////////////////////
+	// Tests concerning setParent
+	///////////////////////////////
+
+	public function test_getParent_ForNodeWithoutParent_ReturnsNull() {
+		$this->assertNull(
+			$this->fixture->getParent()
+		);
+	}
+
+	public function test_setParent_WithGivenParent_SetsThisNodeAsParent() {
+		$childNode = new tx_oelib_Visibility_Node();
+		$childNode->setParent($this->fixture);
+
+		$this->assertSame(
+			$this->fixture,
+			$childNode->getParent()
+		);
+	}
+
+	public function test_setParent_ForNodeWithAlreadySetParentAndGivenParent_ThrowsException() {
+		$this->setExpectedException(
+			'Exception', 'This node already has a parent node.'
+		);
+		$childNode = new tx_oelib_Visibility_Node();
+		$childNode->setParent($this->fixture);
+
+		$childNode->setParent($this->fixture);
+	}
+
+
+	///////////////////////////////////
+	// Tests concerning markAsVisible
+	///////////////////////////////////
+
+	public function test_markAsVisible_ForInvisibleNode_SetsVisibilityTrue() {
+		$this->fixture->markAsVisible();
+
+		$this->assertTrue(
+			$this->fixture->isVisible()
+		);
+	}
+
+	public function test_markAsVisible_ForVisibleNode_SetsVisibilityTrue() {
+		$visibleNode = new tx_oelib_Visibility_Node(true);
+		$visibleNode->markAsVisible();
+
+		$this->assertTrue(
+			$visibleNode->isVisible()
+		);
+
+		$visibleNode->__destruct();
+	}
+
+	public function test_markAsVisible_ForNodeWithParent_MarksParentAsVisible() {
+		$childNode = new tx_oelib_Visibility_Node();
+		$childNode->setParent($this->fixture);
+		$childNode->markAsVisible();
+
+		$this->assertTrue(
+			$this->fixture->isVisible()
+		);
+	}
+
+	public function test_markAsVisible_ForNodeWithParentAndGrandparent_MarksGrandparentNodeAsVisible() {
+		$childNode = new tx_oelib_Visibility_Node();
+		$grandChildNode = new tx_oelib_Visibility_Node();
+		$childNode->setParent($this->fixture);
+		$grandChildNode->setParent($childNode);
+		$grandChildNode->markAsVisible();
+
+		$this->assertTrue(
+			$this->fixture->isVisible()
+		);
+	}
+}
+?>
