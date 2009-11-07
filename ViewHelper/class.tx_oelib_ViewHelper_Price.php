8cf5f6adcba494d1c98f619baf4ff47f65dbc21c
nielspardon <nielspardon@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-11-07 13:57:08 +0000

    [Bug 3492] Feature: Add a price view helper
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/oelib/trunk@26286 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ViewHelper/class.tx_oelib_ViewHelper_Price.php
@@ -0,0 +1,111 @@
+<?php
+/***************************************************************
+* Copyright notice
+*
+* (c) 2009 Niels Pardon (mail@niels-pardon.de)
+* All rights reserved
+*
+* This script is part of the TYPO3 project. The TYPO3 project is
+* free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* The GNU General Public License can be found at
+* http://www.gnu.org/copyleft/gpl.html.
+*
+* This script is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+
+/**
+ * Class 'tx_oelib_ViewHelper_Price' for the 'oelib' extension.
+ *
+ * This class represents a view helper for formatting a price.
+ *
+ * The value (setValue()) and the currency (setCurrencyFromIsoAlpha3Code())
+ * should be set before calling render(). You can use the same instance of this
+ * view helper to render different values in the same currency by changing the
+ * value via setValue().
+ *
+ * @package TYPO3
+ * @subpackage tx_oelib
+ *
+ * @author Niels Pardon <mail@niels-pardon.de>
+ */
+class tx_oelib_ViewHelper_Price {
+	/**
+	 * @var float the value of the price to render
+	 */
+	protected $value = 0.000;
+
+	/**
+	 * @var tx_oelib_Model_Currency the currency of the price to render
+	 */
+	protected $currency = null;
+
+	/**
+	 * Sets the value of the price to render.
+	 *
+	 * @param float $value the value of the price to render, may be negative,
+	 *                     positive or zero
+	 */
+	public function setValue($value) {
+		$this->value = $value;
+	}
+
+	/**
+	 * Sets the currency of the price to render based on the currency's ISO
+	 * alpha 3 code, e.g. "EUR" for Euro, "USD" for US dollars.
+	 *
+	 * @param string $isoAlpha3Code the ISO alpha 3 code of the currency to set,
+	 *                              must not be empty
+	 */
+	public function setCurrencyFromIsoAlpha3Code($isoAlpha3Code) {
+		$this->currency = tx_oelib_MapperRegistry::
+			get('tx_oelib_Mapper_Currency')->findByIsoAlpha3Code($isoAlpha3Code);
+	}
+
+	/**
+	 * Renders the price based on $this->value and $this->currency.
+	 *
+	 * Please call setCurrencyFromIsoAlpha3Code() prior to calling render().
+	 *
+	 * @return string the rendered price
+	 */
+	public function render() {
+		if (!$this->currency) {
+			throw new Exception(
+				'Please set the currency of the price before calling render().'
+			);
+		}
+
+		$result = '';
+
+		if ($this->currency->hasLeftSymbol()) {
+			$result .= $this->currency->getLeftSymbol() . ' ';
+		}
+
+		$result .= number_format(
+			$this->value,
+			$this->currency->getDecimalDigits(),
+			$this->currency->getDecimalSeparator(),
+			$this->currency->getThousandsSeparator()
+		);
+
+		if ($this->currency->hasRightSymbol()) {
+			$result .= ' ' . $this->currency->getRightSymbol();
+		}
+
+		return $result;
+	}
+}
+
+if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/oelib/Model/class.tx_oelib_ViewHelper_Price.php']) {
+	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/oelib/Model/class.tx_oelib_ViewHelper_Price.php']);
+}
+?>
---
+++ tests/tx_oelib_ViewHelper_Price_testcase.php
@@ -0,0 +1,178 @@
+<?php
+/***************************************************************
+* Copyright notice
+*
+* (c) 2009 Niels Pardon (mail@niels-pardon.de)
+* All rights reserved
+*
+* This script is part of the TYPO3 project. The TYPO3 project is
+* free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* The GNU General Public License can be found at
+* http://www.gnu.org/copyleft/gpl.html.
+*
+* This script is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+
+require_once(t3lib_extMgm::extPath('oelib') . 'class.tx_oelib_Autoloader.php');
+
+/**
+ * Testcase for the tx_oelib_ViewHelper_Price class in the 'oelib' extension.
+ *
+ * @package TYPO3
+ * @subpackage oelib
+ * @author Niels Pardon <mail@niels-pardon.de>
+ */
+class tx_oelib_ViewHelper_Price_testcase extends tx_phpunit_testcase {
+	/**
+	 * @var tx_oelib_ViewHelper_Currency
+	 */
+	private $fixture;
+
+	public function setUp() {
+		$this->fixture = new tx_oelib_ViewHelper_Price();
+	}
+
+	public function teardown() {
+		unset($this->fixture);
+	}
+
+	/**
+	 * @test
+	 */
+	public function renderForCurrencyWithLeftSymbolRendersCurrencySymbolLeftOfPrice() {
+		$this->fixture->setValue(123.45);
+		$this->fixture->setCurrencyFromIsoAlpha3Code('EUR');
+
+		$this->assertEquals(
+			'€ 123,45',
+			$this->fixture->render()
+		);
+	}
+
+	/**
+	 * @test
+	 */
+	public function renderForCurrencyWithRightSymbolRendersCurrencySymbolRightOfPrice() {
+		$this->fixture->setValue(123.45);
+		$this->fixture->setCurrencyFromIsoAlpha3Code('CZK');
+
+		$this->assertEquals(
+			'123,45 Kč',
+			$this->fixture->render()
+		);
+	}
+
+	/**
+	 * @test
+	 */
+	public function renderForCurrencyWithoutDecimalDigitsReturnsPriceWithoutDecimalDigits() {
+		$this->fixture->setValue(123.45);
+		$this->fixture->setCurrencyFromIsoAlpha3Code('SDD');
+
+		$this->assertEquals(
+			'sD 123',
+			$this->fixture->render()
+		);
+	}
+
+	/**
+	 * @test
+	 */
+	public function renderForCurrencyWithOneDecimalDigitReturnsPriceWithOneDecimalDigit() {
+		$this->fixture->setValue(123.45);
+		$this->fixture->setCurrencyFromIsoAlpha3Code('MGA');
+
+		$this->assertEquals(
+			'123,5',
+			$this->fixture->render()
+		);
+	}
+
+	/**
+	 * @test
+	 */
+	public function renderForCurrencyWithTwoDecimalDigitsReturnsPriceWithTwoDecimalDigits() {
+		$this->fixture->setValue(123.45);
+		$this->fixture->setCurrencyFromIsoAlpha3Code('EUR');
+
+		$this->assertEquals(
+			'€ 123,45',
+			$this->fixture->render()
+		);
+	}
+
+	/**
+	 * @test
+	 */
+	public function renderForCurrencyWithThreeDecimalDigitsReturnsPriceWithThreeDecimalDigits() {
+		$this->fixture->setValue(123.456);
+		$this->fixture->setCurrencyFromIsoAlpha3Code('KWD');
+
+		$this->assertEquals(
+			'KD 123,456',
+			$this->fixture->render()
+		);
+	}
+
+	/**
+	 * @test
+	 */
+	public function renderForCurrencyWithCommaAsDecimalSeparatorReturnsPriceWithCommaAsDecimalSeparator() {
+		$this->fixture->setValue(123.45);
+		$this->fixture->setCurrencyFromIsoAlpha3Code('EUR');
+
+		$this->assertEquals(
+			'€ 123,45',
+			$this->fixture->render()
+		);
+	}
+
+	/**
+	 * @test
+	 */
+	public function renderForCurrencyWithPointAsDecimalSeparatorReturnsPriceWithPointAsDecimalSeparator() {
+		$this->fixture->setValue(123.45);
+		$this->fixture->setCurrencyFromIsoAlpha3Code('USD');
+
+		$this->assertEquals(
+			'$ 123.45',
+			$this->fixture->render()
+		);
+	}
+
+	/**
+	 * @test
+	 */
+	public function renderForCurrencyWithPointAsThousandsSeparatorReturnsPriceWithPointAsThousandsSeparator() {
+		$this->fixture->setValue(1234.56);
+		$this->fixture->setCurrencyFromIsoAlpha3Code('EUR');
+
+		$this->assertEquals(
+			'€ 1.234,56',
+			$this->fixture->render()
+		);
+	}
+
+	/**
+	 * @test
+	 */
+	public function renderForCurrencyWithCommaAsThousandsSeparatorReturnsPriceWithCommaAsThousandsSeparator() {
+		$this->fixture->setValue(1234.56);
+		$this->fixture->setCurrencyFromIsoAlpha3Code('USD');
+
+		$this->assertEquals(
+			'$ 1,234.56',
+			$this->fixture->render()
+		);
+	}
+}
+?>
