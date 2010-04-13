533c66dd6bdf94bc7d1d2e031129172d56af8eab
oliklee <oliklee@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-04-13 20:40:45 +0000

    [Bug 3773] Add an interface "Geo", r=niels
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/oelib/trunk@32173 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ Interface/class.tx_oelib_Interface_Geo.php
@@ -0,0 +1,91 @@
+<?php
+/***************************************************************
+* Copyright notice
+*
+* (c) 2010 Oliver Klee <typo3-coding@oliverklee.de>
+* All rights reserved
+*
+* This script is part of the TYPO3 project. The TYPO3 project is
+* free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* The GNU General Public License can be found at
+* http://www.gnu.org/copyleft/gpl.html.
+*
+* This script is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+
+/**
+ * Interface tx_oelib_Interface_Geo for the "oelib" extension.
+ *
+ * This class represents an object that can have geo coordinates.
+ *
+ * @package TYPO3
+ * @subpackage tx_oelib
+ *
+ * @author Oliver Klee <typo3-coding@oliverklee.de>
+ */
+interface tx_oelib_Interface_Geo {
+	/**
+	 * Retrieves this object's coordinates.
+	 *
+	 * @return array this object's geo coordinates using the keys "latitude" and
+	 *               "longitude", the coordinates (but not the array) will be
+	 *               empty strings if this object has no coordinates
+	 */
+	public function getGeoCoordinates();
+
+	/**
+	 * Sets this object's coordinates.
+	 *
+	 * @param array $coordinates
+	 *        the coordinates, using the keys "latitude" and "longitude",
+	 *        the array values must not be empty
+	 */
+	public function setGeoCoordinates(array $coordinates);
+
+	/**
+	 * Checks whether this object has non-empty coordinates.
+	 *
+	 * @return boolean TRUE if this object has both a non-empty longitude and
+	 *                 a non-empty latitude, FALSE otherwise
+	 */
+	public function hasGeoCoordinates();
+
+	/**
+	 * Purges this object's geo coordinates.
+	 *
+	 * Note: Calling this function has no influence on this object's geo error
+	 * status.
+	 */
+	public function clearGeoCoordinates();
+
+	/**
+	 * Checks whether there has been a problem with this object's geo
+	 * coordinates.
+	 *
+	 * Note: This function only checks whether there has been an error with the
+	 * coordinates, not whether this object actually has coordinates.
+	 *
+	 * @return boolean TRUE if there has been an error, FALSE otherwise
+	 */
+	public function hasGeoError();
+
+	/**
+	 * Marks this object as having an error with the geo coordinates.
+	 */
+	public function setGeoError();
+
+	/**
+	 * Marks this object as not having an error with the geo coordinates.
+	 */
+	protected function clearGeoError();
+}
+?>
