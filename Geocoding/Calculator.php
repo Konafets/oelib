c5e5eb0946bc01b860c50dd33c62eaba3db8cbc1
oliklee <oliklee@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-08-06 21:35:31 +0000

    [Bug 4030] Add a function for calculating the distance between two	geo objects, r=bernd
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/oelib/trunk@36447 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ Geocoding/Calculator.php
@@ -0,0 +1,92 @@
+<?php
+/***************************************************************
+* Copyright notice
+*
+* (c) 2010 Oliver Klee <typo3-coding@oliverklee.de>
+* All rights reserved
+*
+* This script is part of the TYPO3 project. The TYPO3 project is
+* free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* The GNU General Public License can be found at
+* http://www.gnu.org/copyleft/gpl.html.
+*
+* This script is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+
+/**
+ * Class tx_oelib_Geocoding_Calculator for the "oelib" extension.
+ *
+ * This class provides functions for calculating the distance between geo
+ * objects.
+ *
+ * @package TYPO3
+ * @subpackage tx_oelib
+ *
+ * @author Oliver Klee <typo3-coding@oliverklee.de>
+ */
+class tx_oelib_Geocoding_Calculator {
+	/**
+	 * the earth radius in kilometers
+	 *
+	 * @var float
+	 */
+	const EARTH_RADIUS_IN_KILOMETERS = 6378.7;
+
+	/**
+	 * Calculates the great-circle distance in kilometers between two geo
+	 * objects using the haversine formula.
+	 *
+	 * @param tx_oelib_Interface_Geo $object1
+	 *        the first object, must have geo coordinates
+	 * @param tx_oelib_Interface_Geo $object2
+	 *        the second object, must have geo coordinates
+	 *
+	 * @return float the distance between $object1 and $object2 in kilometers,
+	 *               will be >= 0.0
+	 */
+	public function calculateDistanceInKilometers(
+		tx_oelib_Interface_Geo $object1, tx_oelib_Interface_Geo $object2
+	) {
+		if ($object1->hasGeoError()) {
+			throw new InvalidArgumentException('$object1 has a geo error.');
+		}
+		if ($object2->hasGeoError()) {
+			throw new InvalidArgumentException('$object2 has a geo error.');
+		}
+		if (!$object1->hasGeoCoordinates()) {
+			throw new InvalidArgumentException(
+				'$object1 needs to have coordinates, but has none.'
+			);
+		}
+		if (!$object2->hasGeoCoordinates()) {
+			throw new InvalidArgumentException(
+				'$object2 needs to have coordinates, but has none.'
+			);
+		}
+
+		$coordinates1 = $object1->getGeoCoordinates();
+		$latitude1 = deg2rad($coordinates1['latitude']);
+		$longitude1 = deg2rad($coordinates1['longitude']);
+		$coordinates2 = $object2->getGeoCoordinates();
+		$latitude2 = deg2rad($coordinates2['latitude']);
+		$longitude2 = deg2rad($coordinates2['longitude']);
+
+		return acos(sin($latitude1) * sin($latitude2)
+			+ cos($latitude1) * cos($latitude2) * cos($longitude2 - $longitude1)
+		) * self::EARTH_RADIUS_IN_KILOMETERS;
+	}
+}
+
+if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/oelib/Geocoding/Calculator.php']) {
+	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/oelib/Geocoding/Calculator.php']);
+}
+?>
---
+++ tests/Geocoding/CalculatorTest.php
@@ -0,0 +1,180 @@
+<?php
+/***************************************************************
+* Copyright notice
+*
+* (c) 2010 Oliver Klee (typo3-coding@oliverklee.de)
+* All rights reserved
+*
+* This script is part of the TYPO3 project. The TYPO3 project is
+* free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* The GNU General Public License can be found at
+* http://www.gnu.org/copyleft/gpl.html.
+*
+* This script is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+
+require_once(t3lib_extMgm::extPath('oelib') . 'class.tx_oelib_Autoloader.php');
+
+/**
+ * Testcase for the tx_oelib_Geocoding_Calculator class in the "oelib"
+ * extension.
+ *
+ * @package TYPO3
+ * @subpackage tx_oelib
+ *
+ * @author Oliver Klee <typo3-coding@oliverklee.de>
+ */
+class tx_oelib_Geocoding_CalculatorTest extends tx_phpunit_testcase {
+	/**
+	 * @var tx_oelib_Geocoding_Calculator
+	 */
+	private $fixture;
+
+	public function setUp() {
+		$this->fixture = new tx_oelib_Geocoding_Calculator();
+	}
+
+	public function tearDown() {
+		unset($this->fixture);
+	}
+
+
+	///////////////////////////////////////////////////
+	// Tests concerning calculateDistanceInKilometers
+	///////////////////////////////////////////////////
+
+	/**
+	 * @test
+	 *
+	 * @expectedException InvalidArgumentException
+	 */
+	public function calculateDistanceInKilometersForFirstObjectWithoutCoordinatesThrowsException() {
+		$noCoordinates = new tx_oelib_tests_fixtures_TestingGeo();
+		$noCoordinates->clearGeoCoordinates();
+		$bonn = new tx_oelib_tests_fixtures_TestingGeo();
+		$bonn->setGeoCoordinates(
+			array('latitude' => 50.72254683, 'longitude' => 7.07519531)
+		);
+
+		$this->fixture->calculateDistanceInKilometers($noCoordinates, $bonn);
+	}
+
+	/**
+	 * @test
+	 *
+	 * @expectedException InvalidArgumentException
+	 */
+	public function calculateDistanceInKilometersForSecondObjectWithoutCoordinatesThrowsException() {
+		$bonn = new tx_oelib_tests_fixtures_TestingGeo();
+		$bonn->setGeoCoordinates(
+			array('latitude' => 50.72254683, 'longitude' => 7.07519531)
+		);
+		$noCoordinates = new tx_oelib_tests_fixtures_TestingGeo();
+		$noCoordinates->clearGeoCoordinates();
+
+		$this->fixture->calculateDistanceInKilometers($bonn, $noCoordinates);
+	}
+
+	/**
+	 * @test
+	 *
+	 * @expectedException InvalidArgumentException
+	 */
+	public function calculateDistanceInKilometersForFirstObjectWithGeoErrorThrowsException() {
+		$brokenBonn = new tx_oelib_tests_fixtures_TestingGeo();
+		$brokenBonn->setGeoCoordinates(
+			array('latitude' => 50.72254683, 'longitude' => 7.07519531)
+		);
+		$brokenBonn->setGeoError();
+		$bonn = new tx_oelib_tests_fixtures_TestingGeo();
+		$bonn->setGeoCoordinates(
+			array('latitude' => 50.72254683, 'longitude' => 7.07519531)
+		);
+
+		$this->fixture->calculateDistanceInKilometers($brokenBonn, $bonn);
+	}
+
+	/**
+	 * @test
+	 *
+	 * @expectedException InvalidArgumentException
+	 */
+	public function calculateDistanceInKilometersForSecondObjectWithGeoErrorThrowsException() {
+		$bonn = new tx_oelib_tests_fixtures_TestingGeo();
+		$bonn->setGeoCoordinates(
+			array('latitude' => 50.72254683, 'longitude' => 7.07519531)
+		);
+		$brokenBonn = new tx_oelib_tests_fixtures_TestingGeo();
+		$brokenBonn->setGeoCoordinates(
+			array('latitude' => 50.72254683, 'longitude' => 7.07519531)
+		);
+		$brokenBonn->setGeoError();
+
+		$this->fixture->calculateDistanceInKilometers($bonn, $brokenBonn);
+	}
+
+	/**
+	 * @test
+	 */
+	public function calculateDistanceInKilometersForSameObjectsReturnsZero() {
+		$bonn = new tx_oelib_tests_fixtures_TestingGeo();
+		$bonn->setGeoCoordinates(
+			array('latitude' => 50.72254683, 'longitude' => 7.07519531)
+		);
+
+		$this->assertEquals(
+			0.0,
+			$this->fixture->calculateDistanceInKilometers($bonn, $bonn)
+		);
+	}
+
+	/**
+	 * @test
+	 */
+	public function calculateDistanceInKilometersForBonnAndCologneReturnsActualDistance() {
+		$bonn = new tx_oelib_tests_fixtures_TestingGeo();
+		$bonn->setGeoCoordinates(
+			array('latitude' => 50.72254683, 'longitude' => 7.07519531)
+		);
+		$cologne = new tx_oelib_tests_fixtures_TestingGeo();
+		$cologne->setGeoCoordinates(
+			array('latitude' => 50.94458443, 'longitude' => 6.9543457)
+		);
+
+		$this->assertEquals(
+			26.0,
+			$this->fixture->calculateDistanceInKilometers($bonn, $cologne),
+			'',
+			2.0
+		);
+	}
+
+	/**
+	 * @test
+	 */
+	public function calculateDistanceInKilometersReturnsSameDistanceForSwappedArguments() {
+		$bonn = new tx_oelib_tests_fixtures_TestingGeo();
+		$bonn->setGeoCoordinates(
+			array('latitude' => 50.72254683, 'longitude' => 7.07519531)
+		);
+		$cologne = new tx_oelib_tests_fixtures_TestingGeo();
+		$cologne->setGeoCoordinates(
+			array('latitude' => 50.94458443, 'longitude' => 6.9543457)
+		);
+
+		$this->assertEquals(
+			$this->fixture->calculateDistanceInKilometers($bonn, $cologne),
+			$this->fixture->calculateDistanceInKilometers($cologne, $bonn)
+		);
+	}
+}
+?>
