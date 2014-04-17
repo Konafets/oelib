bd9e0b0c1cc7566767cc9756b5cca4eef0d1fcc1
oliklee <oliklee@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-04-17 21:04:09 +0000

    [#4791] Change: Move the non-subdirectory classes to Classes/
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/oelib/trunk@84911 735d13b6-9817-0410-8766-e36946ffe9aa

--- class.tx_oelib_Double3Validator.php
+++
@@ -1,3 +0,0 @@
-<?php
-// This is just a dummy for cases where the validator has been directly included.
-// The real validator should get loading via the Core autoloader.
--- ext_localconf.php
+++ ext_localconf.php
@@ -6,7 +6,7 @@ if (!defined('TYPO3_MODE')) {
 $GLOBALS['TYPO3_CONF_VARS'][TYPO3_MODE]['XCLASS']['ext/oelib/Tests/Unit/Fixtures/Empty.php']
 	= t3lib_extMgm::extPath('oelib') . 'Tests/Unit/Fixtures/Xclass/Empty.php';
 
-$TYPO3_CONF_VARS['SC_OPTIONS']['tce']['formevals']['tx_oelib_Double3Validator'] = 'EXT:oelib/class.tx_oelib_Double3Validator.php';
+$TYPO3_CONF_VARS['SC_OPTIONS']['tce']['formevals']['tx_oelib_Double3Validator'] = 'EXT:oelib/Classes/Double3Validator.php';
 
 $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['oelib']['testingFrameworkCleanUp'][]
 	= 'EXT:oelib/class.tx_oelib_TestingFrameworkCleanup.php:' .
