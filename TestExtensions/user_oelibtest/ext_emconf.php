936411da11426fe4b8fff5c5ded4e6cdaa897a20
oliklee <oliklee@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-06-20 11:18:33 +0000

    [#5079] Change: Add dependencies for the test extensions
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/oelib/trunk@85681 735d13b6-9817-0410-8766-e36946ffe9aa

--- TestExtensions/user_oelibtest/ext_emconf.php
+++ TestExtensions/user_oelibtest/ext_emconf.php
@@ -34,6 +34,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'depends' => array(
 			'php' => '5.3.0-5.5.99',
 			'typo3' => '4.5.0-6.2.99',
+			'oelib' => '',
 		),
 		'conflicts' => array(
 		),
--- TestExtensions/user_oelibtest2/ext_emconf.php
+++ TestExtensions/user_oelibtest2/ext_emconf.php
@@ -34,6 +34,8 @@ $EM_CONF[$_EXTKEY] = array(
 		'depends' => array(
 			'php' => '5.3.0-5.5.99',
 			'typo3' => '4.5.0-6.2.99',
+			'oelib' => '',
+			'user_oelibtest' => '',
 		),
 		'conflicts' => array(
 		),
