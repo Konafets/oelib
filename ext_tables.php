126755ea2c172b2dfb9dbdb61f3bad457b0fcd21
Stefano Kowalke <blueduck@mailbox.org>
2015-12-28 19:27:36 +0100

    [TASK] Use FQNS in ext_tables.php

--- ext_tables.php
+++ ext_tables.php
@@ -1,9 +1,8 @@
 <?php
-use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 
 defined('TYPO3_MODE') or die('Access denied.');
 
-ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'One is Enough Library');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'One is Enough Library');
 
 $GLOBALS['TCA']['tx_oelib_test'] = array(
 	'ctrl' => array(
@@ -23,8 +22,8 @@ $GLOBALS['TCA']['tx_oelib_test'] = array(
 			'starttime' => 'starttime',
 			'endtime' => 'endtime',
 		),
-		'dynamicConfigFile' => ExtensionManagementUtility::extPath($_EXTKEY) . 'Configuration/TCA/Test.php',
-		'iconfile' => ExtensionManagementUtility::extRelPath($_EXTKEY) . 'Resources/Public/Icons/Test.gif',
+		'dynamicConfigFile' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'Configuration/TCA/Test.php',
+		'iconfile' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath($_EXTKEY) . 'Resources/Public/Icons/Test.gif',
 	),
 );
 
@@ -41,13 +40,13 @@ $GLOBALS['TCA']['tx_oelib_testchild'] = array(
 		'versioningWS' => FALSE,
 		'default_sortby' => 'ORDER BY uid',
 		'delete' => 'deleted',
-		'dynamicConfigFile' => ExtensionManagementUtility::extPath($_EXTKEY) . 'Configuration/TCA/TestChild.php',
-		'iconfile' => ExtensionManagementUtility::extRelPath($_EXTKEY) . 'Resources/Public/Icons/Test.gif',
+		'dynamicConfigFile' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'Configuration/TCA/TestChild.php',
+		'iconfile' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath($_EXTKEY) . 'Resources/Public/Icons/Test.gif',
 	),
 );
 
 $addToFeInterface = (\TYPO3\CMS\Core\Utility\VersionNumberUtility::convertVersionNumberToInteger(TYPO3_version) < 6002000);
-ExtensionManagementUtility::addTCAcolumns(
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns(
 	'fe_users',
 	array(
 		'tx_oelib_is_dummy_record' => array(),
