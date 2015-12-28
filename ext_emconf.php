8a4a2947451d2a68ab9132a73311a0077ed47f03
Stefano Kowalke <blueduck@mailbox.org>
2015-12-28 19:25:48 +0100

    [TASK] Raise TYPO3 version to 7

--- ext_emconf.php
+++ ext_emconf.php
@@ -37,7 +37,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'constraints' => array(
 		'depends' => array(
 			'php' => '5.5.0-7.0.99',
-			'typo3' => '6.2.0-6.2.99',
+			'typo3' => '6.2.0-7.9.99',
 			'static_info_tables' => '2.0.8-',
 		),
 		'conflicts' => array(
